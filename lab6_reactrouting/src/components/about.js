ae8bc8d5ac486755e15d0945de89047fcfc815d1
arvinogega <arvinogega@gmail.com>
2024-07-28 23:37:17 -0400

    React routing

---
+++ lab6_reactrouting/src/components/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About =function(){
+    return(
+        <>
+        <header className="header">
+            <h1 className="headertitle">About Us</h1>
+        </header>
+        </>
+    )
+}
+export default About;
---
+++ lab6_reactrouting/src/components/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home=function(){
+    return(
+    <>
+    <header className="header">
+        <h1 className="headertitle">Home</h1>
+    </header>
+    </>
+    )
+}
+export default Home;
