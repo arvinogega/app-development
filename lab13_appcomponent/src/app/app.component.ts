a98fdab2575ddbcb32c4fb50db3c51e549143299
arvinogega <arvinogega@gmail.com>
2024-08-12 22:22:31 -0400

    lab13_app component

---
+++ lab13_appcomponent/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from './products/products.components';
+import { ItemComponent } from './item/item.component';
+import { BookComponent } from './book/book.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,ProductComponent,ItemComponent,BookComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'first_angluar';
+}
---
+++ lab13_appcomponent/src/app/book/repository.model.ts
@@ -0,0 +1,20 @@
+import { Book } from "./book.model";
+import { Datasource } from "./datasource.model";
+
+export class Bookrepository{
+    private datasource: Datasource
+    private books:Book[]
+
+    constructor (){
+        this.datasource=new Datasource()
+        this.books =new Array<Book>()
+        this.datasource.getBooks().forEach(b=>this.books.push(b))
+    }
+
+    getBooks():Book[]{
+        return this.books
+    }
+    getBookId(id:number):any{
+        return this.books.find(b=> b.id ==id)
+    }
+}
